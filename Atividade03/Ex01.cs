3948536a59ce76035f579ea1aaf03e1f0dc26711
Kauany <kauanylima.sys@gmail.com>
2026-08-11 09:58:22 -0300

    Adicionei a atividade 3

---
+++ Atividade03/Ex01.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Ex01
+{
+    //função de soma dos elementos de um vetor
+    static int SomaDosElementos(int[] vetor)
+    {
+        int soma = 0;
+        for(int i = 0; i < vetor.Length; i++)
+           soma = soma + vetor[i];
+        
+        return soma;
+    }
+    //código principal
+    static void Main()
+    {
+        int n;
+         Console.Write("Digite o tamanho do vetor desejado: ");
+         n = int.Parse(Console.ReadLine());
+
+        int [] vetor = new int[n];
+        for(int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write($"Elemento[{i}]: ");
+            vetor[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine($"A soma dos elementos desse vetor é {SomaDosElementos(vetor)}" );
+
+    }
+}
---
+++ Atividade03/ex02.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Specialized;
+
+class Ex02
+{
+    static int  ContarImpares (int[] vetor)
+    {
+        int  impares = 0;
+        for(int i = 0; i < vetor.Length ; i++)
+        {
+            if (vetor[i] % 2 != 0)
+            {
+                impares = impares + 1;
+            }
+        }
+        return impares;
+    }
+
+    static void Main()
+    {
+        int n ;
+
+        Console.Write("Digite o tamanho do vetor Desejado: ");
+        n = int.Parse(Console.ReadLine());
+
+        int [] vetor = new int[n];
+
+        Console.WriteLine("____Digite os elementos do vetor____");
+
+        for(int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write($"vetor[{i}]: ");
+            vetor[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine($"Esse vetor possui {ContarImpares(vetor)} números ímpares.");
+        
+
+
+
+
+        
+
+
+    }
+}
